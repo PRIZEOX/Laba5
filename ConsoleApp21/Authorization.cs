c48b3326a82588c6aec47cf0667d891e8b23f1e7
Ararat <oganisyanararat1@gmail.com>
2022-12-11 20:09:03 +0400

    Написание комментов

--- ConsoleApp21/Authorization.cs
+++ ConsoleApp21/Authorization.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp21
 {
-    class Class1
+    class Authorization
     {
+        string login;
+        string password;
+        //Метод проверияет логин
+        public static int Metod()
+        {
+            return 0;
+        }
+        //Метод проверияет пароль
+        public static int Meto1d()
+        {
+            return 0;
+        }
     }
 }
